4480bcdd020ddb1fed1f41eeae70496fced24fe5
Maya201111 <157573073+Maya201111@users.noreply.github.com>
2024-03-27 06:52:29 +0000

    i commited this file

---
+++ homework.py
@@ -0,0 +1,7 @@
+word='grape'
+symbol='a'
+for x in word y=x==symbol
+    if(x== symbol()):bool
+    counter=counter+1
+    counter +1
+print('concidents', counter)
--- odessa.py
+++ odessa.py
@@ -1,10 +1,10 @@
 s='Odesssssa'
 new_s=''
-prev_w=:
+prev_w = ...
 
- for w in s:
-    if prev_w != w:
-       new_s += w
-       prev_w = w
+for w in s:
+   if prev_w != w:
+      new_s += w
+      prev_w = w
 print('New word:', new_s)
 
